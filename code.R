60b02cef1209b8e1e327f4b43016b6b3ca64e4e8
XrysaM <xrysa.m99@gmail.com>
2022-12-03 22:02:56 +0200

    tried Linear Discriminant Analysis but have memory issues, it doesnt run. Also cleaned the code a bit, removed extra variables

--- code.R
+++ code.R
@@ -1,7 +1,10 @@
 #This is the code 
 
+#clean environment everytime so run what you need from the start
+#rm(list=ls()) & gc() for cleaning
+
 library(readr)
-k_9 <- read_csv("k_9_total_for_classification.csv")
+k_9_og <- read_csv("k_9_total_for_classification.csv") #.
 summary(k_9)
 View(k_9)
 
@@ -9,46 +12,47 @@ any(is.na(k_9)) #is False == no NAs
 unique(k_9[c(3,4)]) #poia hosts kai posa
 
 #encoders
-rm(k_9_test2)
-k_9_test2 <- k_9
+k_9 <- k_9_og #.
+#rm(k_9_og)   #call read_csv again if you want original(saves space)
 
 #alphabetical order
 library(CatEncoders)
-labs1 = LabelEncoder.fit(k_9_test2$host_categories)
-labs2 = LabelEncoder.fit(k_9_test2$common_species_names)
-k_9_test2$host_categories = transform(labs1, k_9_test2$host_categories)
-k_9_test2$common_species_names = transform(labs2, k_9_test2$common_species_names)
-#dixnei tis arxikes times
-list1 <- unique(inverse.transform(labs1, k_9_test2$host_categories))  
-list2 <- unique(inverse.transform(labs2, k_9_test2$common_species_names))
-ogvalues_host_categories <- list1[order(unlist(list1))]
-ogvalues_common_species_names <- list2[order(unlist(list2))]
+#encode host_categories
+lab = LabelEncoder.fit(k_9$host_categories)
+k_9$host_categories = transform(lab, k_9$host_categories)
+  #dixnei tis arxikes times
+list <- unique(inverse.transform(lab, k_9$host_categories))
+ogvalues_host_categories <- list[order(unlist(list))]
 ogvalues_host_categories
+
+#encode common_species_names - this
+lab = LabelEncoder.fit(k_9$common_species_names)
+k_9$common_species_names = transform(lab, k_9$common_species_names)
+  #dixnei tis arxikes times
+list <- unique(inverse.transform(lab, k_9$common_species_names))
+ogvalues_common_species_names <- list[order(unlist(list))]
 ogvalues_common_species_names
 
 
 #smaller sample size for testing
 library(dplyr)
-testk_9 <- slice_sample(k_9_test2[,-c(1:3)], n = 60 ) #keep common_species_names and kmers
+sample_k9 <- slice_sample(k_9[,-c(1:3)], n = 60 ) #sample size
+k_9_test <- k_9[,-c(1:3)] #keep only common_species_names and kmers
+
 
 #PCA - dimensionality reduction
-k_9_test <- k_9_test2[,-c(1:3)]
-rownames(k_9_test)
 pca <- prcomp(k_9_test , scale = TRUE)
-#plot
+#simple plot, rough not necessary
 plot(pca$x[,1], pca$x[,2])
 ## make a scree plot
 pca.var <- pca$sdev^2
 pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
 barplot(pca.var.per, main="Scree Plot", xlab="Principal Component", ylab="Percent Variation")
-
-#doesnt work :(
+#better plot
 library(ggplot2)
-
 pca.data <- data.frame( X=pca$x[,1], Y=pca$x[,2], 
                         Samples = rownames(k_9_test)) 
 pca.data
-
 hosts <- factor(k_9_test$common_species_names)
 ggplot(data=pca.data, aes(x=X, y=Y, label=Samples, colour= hosts)) +
   geom_point() +
@@ -57,13 +61,30 @@ ggplot(data=pca.data, aes(x=X, y=Y, label=Samples, colour= hosts)) +
   theme_bw() +
   ggtitle("My PCA Graph")
 
-#loading scores
+#loading scores - most important kmers for the variation
 loading_scores <- pca$rotation[,1]
 gene_scores <- abs(loading_scores) ## get the magnitudes
 gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
 top_kmers <- names(gene_score_ranked[1:10]) #how many kmers i want
 
 top_kmers ## show the names of the top 10 genes
-
 pca$rotation[top_kmers,1] ## show the scores (and +/- sign)
 
+
+
+#LDA - Linear Discriminant Analysis  -- doest't work :(
+library(MASS)
+model <- lda(formula = Hosts ~ ., data = k_9_test)
+# get the x,y coordinates for the LDA plot
+data.lda.values <- predict(model)
+# create a dataframe that has all the info we need to draw a graph
+plot.data <- data.frame(X=data.lda.values$x[,1], Y=data.lda.values$x[,2], 
+                        Hosts=k_9_test$common_species_names)
+
+head(plot.data)
+
+# draw a graph using ggplot2
+p <- ggplot(data=plot.data, aes(x=X, y=Y)) +
+  geom_point(aes(color=Hosts)) +
+  theme_bw()
+p
