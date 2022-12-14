c412ffd94a80a8fff2df0ffd2120ee008a567816
XrysaM <xrysa.m99@gmail.com>
2022-12-14 21:20:52 +0200

    added a code for Random forest - alloc error removed dimensionality reduction techniques for now, moved them to a new file "examples dimensionality reduction.R" removed the encoder for the host_categories feature, moved it to "testfile.R"

--- code.R
+++ code.R
@@ -1,30 +1,27 @@
 #This is the code 
 
 #clean environment everytime so run what you need from the start
-#rm(list=ls()) & gc() for cleaning
+#rm(list=ls()) #& gc() for cleaning
 
 library(readr)
 k_9_og <- read_csv("k_9_total_for_classification.csv") #.
+k_9 <- k_9_og #.
+rm(k_9_og)   #call read_csv again if you want original(saves space)
+
 summary(k_9)
+str(k_9)
 View(k_9)
 
+#NA's?
 any(is.na(k_9)) #is False == no NAs
-unique(k_9[c(3,4)]) #poia hosts kai posa
 
-k_9 <- k_9_og #.
-#rm(k_9_og)   #call read_csv again if you want original(saves space)
+#which hosts
+unique(k_9[c(3,4)]) 
+
 
 #encoders -- alphabetical order
 library(CatEncoders)
-#encode host_categories
-lab = LabelEncoder.fit(k_9$host_categories)
-k_9$host_categories = transform(lab, k_9$host_categories)
-  #dixnei tis arxikes times
-list <- unique(inverse.transform(lab, k_9$host_categories))
-ogvalues_host_categories <- list[order(unlist(list))]
-ogvalues_host_categories
-
-#encode common_species_names - this
+#encode common_species_names 
 lab = LabelEncoder.fit(k_9$common_species_names)
 k_9$common_species_names = transform(lab, k_9$common_species_names)
   #dixnei tis arxikes times
@@ -39,52 +36,74 @@ sample_k9 <- slice_sample(k_9[,-c(1:3)], n = 60 ) #sample size
 k_9_test <- k_9[,-c(1:3)] #keep only common_species_names and kmers
 
 
-#PCA - dimensionality reduction
-pca <- prcomp(k_9_test , scale = TRUE)
-#simple plot, rough not necessary
-plot(pca$x[,1], pca$x[,2])
-## make a scree plot
-pca.var <- pca$sdev^2
-pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
-barplot(pca.var.per, main="Scree Plot", 
-        xlab="Principal Component", ylab="Percent Variation")
-#better plot
+#Random Forest try
 library(ggplot2)
-pca.data <- data.frame( X=pca$x[,1], Y=pca$x[,2], 
-                        Samples = rownames(k_9_test)) 
-pca.data
-hosts <- factor(k_9_test$common_species_names)
-ggplot(data=pca.data, aes(x=X, y=Y, label=Samples, colour= hosts)) +
-  geom_point() +
-  xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
-  ylab(paste("PC2 - ", pca.var.per[2], "%", sep="")) +
+library(cowplot)
+library(randomForest)
+set.seed(42)
+
+k_9_test$common_species_names <- as.factor(k_9_test$common_species_names)
+#do i need this?
+#data.imputed <- rfImpute(common_species_names ~ ., data = k_9_test, iter=6) 
+
+model <- randomForest(common_species_names ~ ., data=k_9_test, proximity=TRUE)
+model
+
+oob.error.data <- data.frame(
+  Trees=rep(1:nrow(model$err.rate), times=3),
+  Type=rep(c("OOB", "Healthy", "Unhealthy"), each=nrow(model$err.rate)),
+  Error=c(model$err.rate[,"OOB"], 
+          model$err.rate[,"Healthy"], 
+          model$err.rate[,"Unhealthy"]))
+
+ggplot(data=oob.error.data, aes(x=Trees, y=Error)) +
+  geom_line(aes(color=Type))
+
+
+## If we want to compare this random forest to others with different values for
+## mtry (to control how many variables are considered at each step)...
+oob.values <- vector(length=10)
+for(i in 1:10) {
+  temp.model <- randomForest(hd ~ ., data=data.imputed, mtry=i, ntree=1000)
+  oob.values[i] <- temp.model$err.rate[nrow(temp.model$err.rate),1]
+}
+oob.values
+## find the minimum error
+min(oob.values)
+## find the optimal value for mtry...
+which(oob.values == min(oob.values))
+## create a model for proximities using the best value for mtry
+model <- randomForest(hd ~ ., 
+                      data=data.imputed,
+                      ntree=1000, 
+                      proximity=TRUE, 
+                      mtry=which(oob.values == min(oob.values)))
+
+
+## Now let's create an MDS-plot to show how the samples are related to each 
+## other.
+##
+## Start by converting the proximity matrix into a distance matrix.
+distance.matrix <- as.dist(1-model$proximity)
+
+mds.stuff <- cmdscale(distance.matrix, eig=TRUE, x.ret=TRUE)
+
+## calculate the percentage of variation that each MDS axis accounts for...
+mds.var.per <- round(mds.stuff$eig/sum(mds.stuff$eig)*100, 1)
+
+## now make a fancy looking plot that shows the MDS axes and the variation:
+mds.values <- mds.stuff$points
+mds.data <- data.frame(Sample=rownames(mds.values),
+                       X=mds.values[,1],
+                       Y=mds.values[,2],
+                       Status=data.imputed$hd)
+
+ggplot(data=mds.data, aes(x=X, y=Y, label=Sample)) + 
+  geom_text(aes(color=Status)) +
   theme_bw() +
-  ggtitle("My PCA Graph")
-
-#loading scores - most important kmers for the variation
-loading_scores <- pca$rotation[,1]
-gene_scores <- abs(loading_scores) ## get the magnitudes
-gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
-top_kmers <- names(gene_score_ranked[1:10]) #how many kmers i want
-
-top_kmers ## show the names of the top 10 kmers
-pca$rotation[top_kmers,1] ## show the scores (and +/- sign)
+  xlab(paste("MDS1 - ", mds.var.per[1], "%", sep="")) +
+  ylab(paste("MDS2 - ", mds.var.per[2], "%", sep="")) +
+  ggtitle("MDS plot using (1 - Random Forest Proximities)")
+# ggsave(file="random_forest_mds_plot.pdf")
 
 
-
-#LDA - Linear Discriminant Analysis  -- doest't work :(
-library(MASS)
-model <- lda(formula = Hosts ~ ., data = k_9_test) #"cannot allocate vector of size 64GB"
-# get the x,y coordinates for the LDA plot
-data.lda.values <- predict(model)
-# create a dataframe that has all the info we need to draw a graph
-plot.data <- data.frame(X=data.lda.values$x[,1], Y=data.lda.values$x[,2], 
-                        Hosts=k_9_test$common_species_names)
-
-head(plot.data)
-
-# draw a graph using ggplot2
-p <- ggplot(data=plot.data, aes(x=X, y=Y)) +
-  geom_point(aes(color=Hosts)) +
-  theme_bw()
-p
