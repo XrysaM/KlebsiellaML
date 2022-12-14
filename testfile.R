aeae766580dbd26e7db6cf1aee93ccf9f5745753
XrysaM <xrysa.m99@gmail.com>
2022-12-14 21:16:55 +0200

    added the encoder for transforming the host_categories column

--- testfile.R
+++ testfile.R
@@ -8,6 +8,13 @@ k_9_test <- k_9[65:85, 3:4 ]
 #simple
 k_9_test$host_categories <- as.numeric(factor(k_9_test$host_categories)) #den 3erw poies times einai poio host
 
+#works 
+k_9_test$host_categories <- as.factor(k_9_test$host_categories)
+k_9_test$common_species_names <- as.factor(k_9_test$common_species_names)
+k_9_test <- data.frame(k_9_test)
+k_9_test$host_categories <- encode_ordinal(k_9_test[,1,drop=FALSE], order = c('human','birds', 'pig'), out.int = TRUE)
+k_9_test$common_species_names <- encode_ordinal(k_9_test[,2,drop=FALSE], order = c('human','turkey', 'pig'), out.int = TRUE)
+
 #the best 
 #alphabetical order
 library(CatEncoders)
@@ -16,11 +23,14 @@ k_9_test$host_categories = transform(labs, k_9_test$host_categories)
 #dixnei tis arxikes times
 unique(inverse.transform(labs, k_9_test$host_categories)) 
 
-#works 
-k_9_test$host_categories <- as.factor(k_9_test$host_categories)
-k_9_test$common_species_names <- as.factor(k_9_test$common_species_names)
-k_9_test <- data.frame(k_9_test)
-k_9_test$host_categories <- encode_ordinal(k_9_test[,1,drop=FALSE], order = c('human','birds', 'pig'), out.int = TRUE)
-k_9_test$common_species_names <- encode_ordinal(k_9_test[,2,drop=FALSE], order = c('human','turkey', 'pig'), out.int = TRUE)
+#encode host_categories
+lab = LabelEncoder.fit(k_9$host_categories)
+k_9$host_categories = transform(lab, k_9$host_categories)
+#dixnei tis arxikes times
+list <- unique(inverse.transform(lab, k_9$host_categories))
+ogvalues_host_categories <- list[order(unlist(list))]
+ogvalues_host_categories
+
+
 
 
