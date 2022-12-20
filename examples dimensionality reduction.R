c198150022d6df59450f97b05ca3376a4e278b76
XrysaM <xrysa.m99@gmail.com>
2022-12-20 21:32:15 +0200

    added an encoderfor the hosts to run pca changed the some values because of the new df (after boruta) added QDA but it probably wontrun for my dataset tried MDS-PCoA but it wont work for the data before the classifier. (there is an MDS for the Random Forest results in "code.R")

--- examples dimensionality reduction.R
+++ examples dimensionality reduction.R
@@ -1,8 +1,20 @@
+
 #dimensionality reduction examples
 
+###############
 
 #PCA - 
-pca <- prcomp(k_9_test , scale = TRUE)
+#encode host_categories
+library(CatEncoders)
+k_9_saved <- k_9_fix
+lab = LabelEncoder.fit(k_9_fix$host_categories)
+k_9_fix$host_categories = transform(lab, k_9_fix$host_categories)
+#dixnei tis arxikes times
+list <- unique(inverse.transform(lab, k_9_fix$host_categories))
+ogvalues_host_categories <- list[order(unlist(list))]
+ogvalues_host_categories
+
+pca <- prcomp(k_9_fix , scale = TRUE)
 #simple plot, rough not necessary
 plot(pca$x[,1], pca$x[,2])
 ## make a scree plot
@@ -13,9 +25,9 @@ barplot(pca.var.per, main="Scree Plot",
 #better plot
 library(ggplot2)
 pca.data <- data.frame( X=pca$x[,1], Y=pca$x[,2], 
-                        Samples = rownames(k_9_test)) 
+                        Samples = rownames(k_9_fix)) 
 pca.data
-hosts <- factor(k_9_test$common_species_names)
+hosts <- factor(k_9_fix$host_categories)
 ggplot(data=pca.data, aes(x=X, y=Y, label=Samples, colour= hosts)) +
   geom_point() +
   xlab(paste("PC1 - ", pca.var.per[1], "%", sep="")) +
@@ -27,21 +39,24 @@ ggplot(data=pca.data, aes(x=X, y=Y, label=Samples, colour= hosts)) +
 loading_scores <- pca$rotation[,1]
 gene_scores <- abs(loading_scores) ## get the magnitudes
 gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
-top_kmers <- names(gene_score_ranked[1:10]) #how many kmers i want
 
+top_kmers <- names(gene_score_ranked[1:100]) #how many kmers i want
 top_kmers ## show the names of the top 10 kmers
 pca$rotation[top_kmers,1] ## show the scores (and +/- sign)
 
+k_9_fix <- k_9_saved
+rm(k_9_saved)
 
+##############
 
-#LDA - Linear Discriminant Analysis  -- doest't work :(
+#LDA - Linear Discriminant Analysis  
 library(MASS)
-model <- lda(formula = Hosts ~ ., data = k_9_test) #"cannot allocate vector of size 64GB"
+model <- lda(formula = host_categories ~ ., data = k_9_fix) 
 # get the x,y coordinates for the LDA plot
 data.lda.values <- predict(model)
 # create a dataframe that has all the info we need to draw a graph
 plot.data <- data.frame(X=data.lda.values$x[,1], Y=data.lda.values$x[,2], 
-                        Hosts=k_9_test$common_species_names)
+                        Hosts=k_9_fix$host_categories)
 
 head(plot.data)
 
@@ -50,3 +65,24 @@ p <- ggplot(data=plot.data, aes(x=X, y=Y)) +
   geom_point(aes(color=Hosts)) +
   theme_bw()
 p
+
+
+#QDA - Quadratic Discriminant Analysis -- Error :some group is too small for 'qda'
+library(MASS)
+# Fit the model
+model <- qda(host_categories~., data = k_9_fix)
+model
+# Make predictions
+predictions <- model %>% predict(test.transformed)
+# Model accuracy
+mean(predictions$class == test.transformed$Species)
+
+
+########
+
+#MDS - PCoA 
+#classical Multi Dimensional scaling - Principal Coordinate Analysis
+#same as PCA but calculate distances instead of correlations between samples.
+
+#it doesnt work with negative values
+
